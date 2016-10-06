eac890e5434a37e309a7d2b0c98cc3ce80d121ec
Dan Nixon <dan@dan-nixon.com>
2016-10-06 11:37:29 +0100

    code for lessons 6 and 7

--- Lessons/Lesson6/main.cpp
+++ Lessons/Lesson6/main.cpp
@@ -1,4 +1,39 @@
+#include <iostream>
+
+int global_variable = 50;
+
+void some_function()
+{
+  int a = 400;
+  char letter = 'b';
+}
+
 int main()
 {
+  std::cout << "global variable contains the value " << global_variable << '\n';
+
+  int a = 100;
+  int *b;
+
+  {
+    int a = 200;
+
+    std::cout << "the value in a is " << a << '\n';
+
+    int *local_ptr = new int(300);
+
+    b = local_ptr;
+  }
+
+  std::cout << "the value in a is " << a << '\n';
+
+  std::cout << "the value pointed to by 'b' is " << *b << '\n';
+  delete b;
+  b = nullptr;
+
+  std::cout << "output some data\n";
+
+  some_function();
+
   return 0;
 }
--- Lessons/Lesson7/main.cpp
+++ Lessons/Lesson7/main.cpp
@@ -1,4 +1,47 @@
+#include <iostream>
+
+void swap(int x, int y)
+{
+  int temp = x;
+  x = y;
+  y = temp;
+}
+
+void swap(int *x, int *y)
+{
+  int temp = *x;
+  *x = *y;
+  *y = temp;
+}
+
+void swap_ref(int &x, int &y)
+{
+  int temp = x;
+  x = y;
+  y = temp;
+}
+
 int main()
 {
+  int a, b;
+
+  std::cout << "Please type in a number\n";
+  std::cin >> a;
+  std::cout << "Please type in another number\n";
+  std::cin >> b;
+
+  std::cout << "before swap, a = " << a << " and b = " << b << '\n';
+  swap(a, b);
+  std::cout << "after swap (by value), a = " << a << " and b = " << b << '\n';
+  swap(&a, &b);
+  std::cout << "after swap by pointer reference, a = " << a << " and b = " << b
+            << '\n';
+
+  swap_ref(a, b);
+  std::cout << "after swap using references, a = " << a << " and b = " << b
+            << '\n';
+
+  int &a_ref = a;
+
   return 0;
 }
