db3ad96c9e89b8a3f4d90178b0a87745340efef3
Dan Nixon <dan@dan-nixon.com>
2016-10-10 11:33:04 +0100

    lesson 8 part 1 & 2

--- Lessons/Lesson8/main.cpp
+++ Lessons/Lesson8/main.cpp
@@ -1,48 +1,64 @@
 #include <iostream>
 
-float calc_avg(const int *arr, const int val);
+size_t findSpace(const char *str);
+void printStr(const char *str);
+size_t strCopy(char *dest, size_t destLen, const char *src, size_t srcLen);
 
 int main(void)
 {
-  const int CONST_VALUE = 3;
-  float ave;
+  char *name = "Dan Nixon";
+  size_t spaceIdx = findSpace(name);
+  std::cout << "name = ";
+  printStr(name);
 
-  // stack based 1-d arrays
-  int array_nums[CONST_VALUE] = {2, 4, 5};
+  size_t firstLen = spaceIdx + 1;
+  size_t lastLen = strlen(name) - spaceIdx;
 
-  // print out the first element
-  std::cout << "value at index 0 is " << array_nums[0] << "\n";
+  char *first = new char[firstLen];
+  strCopy(first, firstLen, name, spaceIdx);
 
-  // stack based 2-d arrays
-  char naughts_n_xs[3][3];
+  char *last = new char[lastLen];
+  strCopy(last, lastLen, name + spaceIdx + 1, strlen(name) - spaceIdx);
 
-  // set the middle square to x
-  naughts_n_xs[1][1] = 'X';
-
-  // arrays on the heap 1-d
-  int *sum = new int[CONST_VALUE];
-
-  for (int i = 0; i < CONST_VALUE; ++i)
-  {
-    std::cout << "enter a value...\n";
-    std::cin >> sum[i];
-  }
-
-  ave = calc_avg(sum, CONST_VALUE);
-
-  std::cout << "the average is " << ave << "\n";
-
-  delete[] sum;
+  std::cout << "first = ";
+  printStr(first);
+  std::cout << "last = ";
+  printStr(last);
 
   return 0;
 }
 
-float calc_avg(const int *arr, const int val)
+size_t findSpace(const char *str)
 {
-  int sum = 0;
+  size_t i;
+  for (i = 0; i < strlen(str); i++)
+  {
+    if (str[i] == ' ')
+      break;
+  }
+  return i;
+}
 
-  for (int i = 0; i < val; ++i)
-    sum += arr[i];
+void printStr(const char *str)
+{
+  std::cout << '\"';
+  for (size_t i = 0; i < strlen(str); i++)
+    std::cout << str[i];
+  std::cout << "\"\n";
+}
 
-  return (sum / (float)val);
+size_t strCopy(char *dest, size_t destLen, const char *src, size_t srcLen)
+{
+  char *destEnd = dest + destLen - 1;
+  const char *srcEnd = src + srcLen;
+
+  size_t i = 0;
+  while (dest != destEnd && src != srcEnd)
+  {
+    *(dest++) = *(src++);
+    i++;
+  }
+  *dest = '\0';
+
+  return i;
 }
