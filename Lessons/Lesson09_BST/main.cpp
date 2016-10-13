2f2489e9d1b673e117c38e456d4deeb4da9a8cb9
Dan Nixon <dan@dan-nixon.com>
2016-10-13 13:25:06 +0100

    make profiling easier

--- Lessons/Lesson09_BST/main.cpp
+++ Lessons/Lesson09_BST/main.cpp
@@ -7,18 +7,22 @@
 
 int main(void)
 {
-  BST<TYPE> bst;
+  BST<TYPE> *bst = new BST<TYPE>();
 
   std::vector<TYPE> bstVals = {50, 70, 20, 90, 100, 80, 0, 30, 60, 10};
-  for (auto it = bstVals.begin(); it != bstVals.end(); ++it)
-    bst.insert(*it);
 
-  bst.print(std::cout);
+  for (auto it = bstVals.begin(); it != bstVals.end(); ++it)
+    bst->insert(*it);
+
+  bst->print(std::cout);
 
   std::vector<TYPE> bstSearchVals = {10, 75, -65, 100, 0};
   for (auto it = bstSearchVals.begin(); it != bstSearchVals.end(); ++it)
     std::cout << "Contains " << *it << "? "
-              << (bst.search(*it) ? "true" : "false") << '\n';
+              << (bst->search(*it) ? "true" : "false") << '\n';
+
+  delete bst;
+  bst = nullptr;
 
   return 0;
 }
