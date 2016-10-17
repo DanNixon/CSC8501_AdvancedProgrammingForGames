6ee4839a8b6cb07f823996d3a0312bbc6a6f268a
Dan Nixon <dan@dan-nixon.com>
2016-10-17 11:23:07 +0100

    auto demo

--- Lessons/Lesson13/main.cpp
+++ Lessons/Lesson13/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
+
+class CA
+{
+public:
+  std::string val;
+};
+
+class CB
+{
+public:
+  size_t val;
+};
 
 int main(void)
 {
+  auto v1 = new CA();
+  v1->val = "Hello, World!";
+  std::cout << v1->val << '\n';
+
+  //v1 = new CB();
+
   return 0;
 }
