6f0fe00d30e9f4c90dffb24584f85f03773febbf
Dan Nixon <dan@dan-nixon.com>
2016-10-18 13:55:42 +0100

    empty tests

---
+++ Coursework/CircuitSimulatorLibTest/unittest1.cpp
@@ -0,0 +1,17 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CircuitSimulator
+{
+namespace Test
+{
+TEST_CLASS(UnitTest1)
+{
+public:
+  TEST_METHOD(TestMethod1)
+  {
+  }
+};
+}
+}
---
+++ Coursework/CommandLineInterfaceLibTest/CLITest.cpp
@@ -0,0 +1,21 @@
+#include "CppUnitTest.h"
+
+#include "CommandLineInterfaceLib/CLI.h"
+#include "CommandLineInterfaceLib/Command.h"
+#include "CommandLineInterfaceLib/SubCommand.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CommandLineInterface
+{
+namespace Test
+{
+TEST_CLASS(CLITest)
+{
+public:	
+  TEST_METHOD(TestMethod2)
+  {
+  }
+};
+}
+}
