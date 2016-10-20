89c7dd41063c1c7d17427eb7f2efa82ced850ba0
Dan Nixon <dan@dan-nixon.com>
2016-10-20 22:03:11 +0100

    more tests

--- Coursework/CircuitSimulatorLibTest/CircuitTest.cpp
+++ Coursework/CircuitSimulatorLibTest/CircuitTest.cpp
@@ -2,6 +2,7 @@
 
 #include "CircuitSimulatorLib/Circuit.h"
 #include "CircuitSimulatorLib/XORGate.h"
+#include "CircuitSimulatorLib/RegisterArray.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -89,6 +90,42 @@ public:
     c.wireUp("xor1.z", "output_bus.out1");
     c.wireUp("xor2.z", "output_bus.out2");
     c.wireUp("input_bus.in3", "output_bus.out3");
+
+    Assert::IsFalse(c.validate());
+  }
+
+  TEST_METHOD(Circuit_Validation_BusCycle)
+  {
+    Circuit c({ "in1", "in2", "in3" }, { "out1", "out2", "out3" });
+
+    c.wireUp("input_bus.in1", "input_bus.in2");
+    c.wireUp("input_bus.in2", "output_bus.out1");
+
+    Assert::IsFalse(c.validate());
+  }
+
+  TEST_METHOD(Circuit_Validation_RegisterArrayCycle)
+  {
+    Circuit c({ "in1", "in2", "in3" }, { "out1", "out2", "out3" });
+
+    c.addComponent(new RegisterArray("r", 2));
+
+    c.wireUp("input_bus.in1", "r.bit_0");
+    c.wireUp("r.bit_0", "r.bit_1");
+    c.wireUp("r.bit_1", "output_bus.out1");
+
+    Assert::IsFalse(c.validate());
+  }
+
+  TEST_METHOD(Circuit_Validation_RegisterArrayShortCycle)
+  {
+    Circuit c({ "in1", "in2", "in3" }, { "out1", "out2", "out3" });
+
+    c.addComponent(new RegisterArray("r", 2));
+
+    c.wireUp("input_bus.in1", "r.bit_0");
+    c.wireUp("r.bit_1", "r.bit_1");
+    c.wireUp("r.bit_0", "output_bus.out1");
 
     Assert::IsFalse(c.validate());
   }
