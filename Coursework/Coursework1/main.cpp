d95b80b52d1adad0020eba7d41c0ae03cd529400
Dan Nixon <dan@dan-nixon.com>
2016-10-25 14:44:53 +0100

    ensure RNG is seeded

--- Coursework/Coursework1/main.cpp
+++ Coursework/Coursework1/main.cpp
@@ -1,11 +1,13 @@
 /** @file */
 
 #include <iostream>
+#include <ctime>
 
 #include "Coursework1Lib/CW1CommandLine.h"
 
 int main()
 {
+  std::srand(std::time(0));
   Coursework1::CW1CommandLine cli(std::cin, std::cout);
   cli.initCLI();
   return cli.run();
--- Coursework/Coursework2/main.cpp
+++ Coursework/Coursework2/main.cpp
@@ -1,11 +1,13 @@
 /** @file */
 
 #include <iostream>
+#include <ctime>
 
 #include "Coursework2Lib/CW2CommandLine.h"
 
 int main()
 {
+  std::srand(std::time(0));
   Coursework2::CW2CommandLine cli(std::cin, std::cout);
   cli.initCLI();
   return cli.run();
