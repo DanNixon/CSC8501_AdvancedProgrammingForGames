51b74da539195d251a81898164d0bf269f598bc4
Dan Nixon <dan@dan-nixon.com>
2016-10-26 11:49:03 +0100

    viterbi wip

--- Coursework/Coursework2Lib/Viterbi.h
+++ Coursework/Coursework2Lib/Viterbi.h
@@ -1,9 +1,11 @@
-/** @file */
+﻿/** @file */
 
 #pragma once
 
 #include <map>
 #include <vector>
+
+// Derived from https://en.wikipedia.org/wiki/Viterbi_algorithm#Pseudocode
 
 namespace Coursework2
 {
@@ -21,7 +23,46 @@ public:
                        const TransitionMatrix &a, const EmissionMatrix &b,
                        const InitialProbability &pi)
   {
-    // TODO
+    size_t tLen = s.size();
+
+    double ** t1 = new double*[tlen];
+    double ** t2 = new double*[tlen];
+
+    for (size_t i = 0; i < tlen; i++)
+    {
+      t1[i] = new double[tlen];
+      t2[i] = new double[tlen];
+    }
+
+    // For each state s_i
+    for (size_t i = 0; i < s.size(); i++)
+    {
+      t1[i][0] = pi.at(s.at(i)) * b.at({s.at(i), o.at(0)});
+      t2[i][0] = 0.0;
+    }
+
+    // For elements of T
+    for (size_t i = 1; i < tLen; i++)
+    {
+      // For each state s_j
+      for (size_t j = 0; j < s.size(); j++)
+      {
+        // TODO
+        t1[j][i] = 0.0;
+        t2[j][i] = 0.0;
+      }
+    }
+
+    // TODO: z_{T}\gets \arg \max _{k}{(T_{1}[k,T])}
+
+    // TODO: xT <- szT
+
+    for (size_t i = tLen; i > 0; i--)
+    {
+      // TODO
+    }
+
+    // TODO: Delete t1 and t2
   }
 };
 }
--- Coursework/Coursework2LibTest/ViterbiTest.cpp
+++ Coursework/Coursework2LibTest/ViterbiTest.cpp
@@ -15,12 +15,41 @@ TEST_CLASS(ViterbiTest)
 public:
   TEST_METHOD(Viterbi_Example)
   {
+    /* The exmaple from https://en.wikipedia.org/wiki/Viterbi_algorithm#Example */
+
     typedef Viterbi<std::string, std::string> StringViterbi;
 
-    StringViterbi::Observations observationSpace = { "", "" };
-    StringViterbi::States stateSpace = { "", "" };
+    StringViterbi::States stateSpace = { "Healthy", "Feaver" };
+    StringViterbi::Observations observations = { "normal", "cold", "dizzy" };
 
-    // TODO
+    StringViterbi::InitialProbability startProbability = {
+      {"Healthy", 0.6},
+      {"Feaver", 0.4}
+    };
+
+    StringViterbi::TransitionMatrix transMatrix = {
+      { { "Healthy", "Healthy" }, 0.7 },
+      { { "Healthy", "Feaver" }, 0.3 },
+      { { "Feaver", "Healthy" }, 0.4 },
+      { { "Feaver", "Healthy" }, 0.6 }
+    };
+
+    StringViterbi::EmissionMatrix emmMatrix = {
+      { { "Healthy", "normal" }, 0.5 },
+      { { "Healthy", "cold" }, 0.4 },
+      { { "Healthy", "dizzy" }, 0.1 },
+      { { "Feaver", "normal" }, 0.0 },
+      { { "Feaver", "cold" }, 0.3 },
+      { { "Feaver", "dizzy" }, 0.6 },
+    };
+    
+    StringViterbi::States result;
+    StringViterbi::FindPath(result, observations, stateSpace, observations, transMatrix, emmMatrix, startProbability);
+
+    Assert::AreEqual((size_t)3, result.size());
+    Assert::AreEqual(std::string("Healthy"), result[0]);
+    Assert::AreEqual(std::string("Healthy"), result[1]);
+    Assert::AreEqual(std::string("Feaver"), result[2]);
   }
 };
 }
